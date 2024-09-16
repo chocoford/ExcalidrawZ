8355c755ca2dcbcae3d39f36b85a841b4d5900da
Chocoford <28218759+chocoford@users.noreply.github.com>
2024-09-16 22:12:55 +0800

    remove excalidraw core files

--- ExcalidrawZ/Resources/excalidraw-latest/assets/channel-D_zuO1lI.js
+++
@@ -1,2 +0,0 @@
-import{al as o,am as n}from"./index-DlKBRLQc.js";const l=(a,r)=>o.lang.round(n.parse(a)[r]);export{l as c};
-//# sourceMappingURL=channel-D_zuO1lI.js.map
--- ExcalidrawZ/Resources/excalidraw-latest/assets/clone-BX48Xuqj.js
+++
@@ -1,2 +0,0 @@
-import{a as r}from"./graph-CxtZTZ82.js";var a=4;function n(o){return r(o,a)}export{n as c};
-//# sourceMappingURL=clone-BX48Xuqj.js.map
