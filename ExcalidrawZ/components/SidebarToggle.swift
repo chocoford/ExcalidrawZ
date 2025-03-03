6ebf2cadffcccd860966ee076205cdf303b5f3e6
Chocoford <28218759+chocoford@users.noreply.github.com>
2025-03-04 01:51:16 +0800

    Create SidebarToggle.swift

---
+++ ExcalidrawZ/components/SidebarToggle.swift
@@ -0,0 +1,76 @@
+//
+//  SidebarToggle.swift
+//  ExcalidrawZ
+//
+//  Created by Dove Zachary on 3/3/25.
+//
+
+import SwiftUI
+
+import ChocofordUI
+
+@available(macOS 13.0, *)
+struct SidebarToggle: View {
+    @EnvironmentObject var appPreference: AppPreference
+    
+    @Binding var columnVisibility: NavigationSplitViewVisibility
+    
+    init(columnVisibility: Binding<NavigationSplitViewVisibility>) {
+        self._columnVisibility = columnVisibility
+    }
+    
+    var body: some View {
+        Menu {
+            Picker(selection: $appPreference.sidebarMode) {
+                Text(.localizable(.sidebarShowAll)).tag(AppPreference.SidebarMode.all)
+                Text(.localizable(.sidebarShowFilesOnly)).tag(AppPreference.SidebarMode.filesOnly)
+            } label: {
+                EmptyView()
+            }
+            .pickerStyle(.inline)
+//            Button {
+//                withAnimation { columnVisibility = .all }
+//                appPreference.sidebarMode = .all
+//            } label: {
+//                if appPreference.sidebarMode == .all && columnVisibility != .detailOnly {
+//                    Image(systemSymbol: .checkmark).font(.footnote)
+//                }
+//                Text(.localizable(.sidebarShowAll))
+//            }
+//            Button {
+//                withAnimation { columnVisibility = .all }
+//                appPreference.sidebarMode = .filesOnly
+//            } label: {
+//                if appPreference.sidebarMode == .filesOnly && columnVisibility != .detailOnly {
+//                    Image(systemSymbol: .checkmark).font(.footnote)
+//                }
+//                Text(.localizable(.sidebarShowFilesOnly))
+//            }
+        } label: {
+            Label("Toggle sidebar", systemSymbol: .sidebarLeading)
+        } primaryAction: {
+            toggleSidebar()
+        }
+        .buttonStyle(.borderless)
+        .onChange(of: appPreference.sidebarMode) { _ in
+            withAnimation { columnVisibility = .all }
+        }
+    }
+    
+    private func toggleSidebar() {
+        withAnimation {
+            if columnVisibility == .detailOnly {
+                columnVisibility = .all
+            } else {
+                columnVisibility = .detailOnly
+            }
+        }
+    }
+}
+
+#Preview {
+    if #available(macOS 13.0, *) {
+        SidebarToggle(columnVisibility: .constant(.all))
+            .environmentObject(AppPreference())
+    }
+}
